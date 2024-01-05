5a35953ec6633f5534aa76a3b81655e5ea75be24
Suvankar Das <hi.suvankar@gmail.com>
2024-01-05 13:05:12 +0530

    placing html into own seperate folder with searchbox

--- app.vue
+++ app.vue
@@ -1,27 +1,3 @@
 <template>
-  <div>
-    <!-- navbar start-->
-    <header
-      class="sticky top-0 z-50 flex justify-between items-center space-x-1 shadow-md border-b p-4"
-    >
-      <a href="" class="text-3xl font-mono">Car Trader</a>
-    </header>
-    <!-- navbar endz -->
-
-
-    <!-- Home Hero -->
-    <div class="relative h-screen w-100 bg-no-repeat bg-cover bg-bottom bg-[url('https://cdn.pixabay.com/photo/2012/05/29/00/43/car-49278_1280.jpg')]">
-      <div class="z-10 absolute w-full h-full flex justify-center items-center">
-        <div class="text-center w-1/2 ">
-          <h1 class="text-8xl font-serif mb-10 text-white txt-big-custom">Find your Car</h1>
-        </div>
-      </div>
-      <!-- blackish background -->
-    <div class="z-1 absolute w-full h-full bg-black opacity-50">
-
-    </div>
-     <!-- blackish background end-->
-    </div>
-    <!-- Home Hero end -->
-  </div>
+  <div></div>
 </template>
