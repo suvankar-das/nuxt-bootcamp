a55c6afd8e77b828055a335960ebcb481eec17e8
Suvankar Das <hi.suvankar@gmail.com>
2024-01-08 21:19:21 +0530

    using useState composable api

--- components/Car/Card.vue
+++ components/Car/Card.vue
@@ -1,31 +1,36 @@
 <script setup>
-
+import heartFilled from "@/assets/images/002-heartFilled.png";
+import heartOutlined from "@/assets/images/002-heartOutline.png";
 
 const props = defineProps({
   carObj: Object,
 });
 
-
-const navigateToCarDetails = ()=>{
+const navigateToCarDetails = () => {
   const routeUrl = `/car/${props.carObj.name}-${props.carObj.id}`;
   navigateTo(routeUrl);
-}
+};
 
+// useState compasable is recommended than ref()
+// it take 2 param , uique key and function that will return initial value
+const favourite = useState(`favourite-${props.carObj.id}`, () => {
+  return false;
+});
 </script>
 
 
 <template>
   <div>
     <div
-      class="shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]"
-      @click="navigateToCarDetails"
+      class="relative shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]"
     >
-      <div class="flex h-full">
-        <NuxtImg
-          :src="carObj.url"
-          alt="car image"
-          class="w-[300px] h-full"
-        />
+      <img
+        class="absolute w-7 right-5 top-3 z-50"
+        :src="favourite ? heartFilled : heartOutlined"
+        @click="favourite = !favourite"
+      />
+      <div class="flex h-full" @click="navigateToCarDetails">
+        <NuxtImg :src="carObj.url" alt="car image" class="w-[300px] h-full" />
         <div class="p-4 flex flex-col">
           <div>
             <h1 class="text-2xl text-blue-700">{{ carObj?.name }}</h1>
