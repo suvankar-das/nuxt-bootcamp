3bf16a4178178cec256d3421d711497ac93e7706
Suvankar Das <hi.suvankar@gmail.com>
2024-01-08 19:34:02 +0530

    update modal

--- components/Car/SideBar.vue
+++ components/Car/SideBar.vue
@@ -1,3 +1,28 @@
+<script setup>
+const modal = ref({
+  location: false,
+  make: false,
+  price: false,
+});
+
+const route = useRoute();
+
+const cityName = ref("");
+
+const toggleModal = (key) => {
+  modal.value[key] = !modal.value[key];
+};
+
+const updateModal = () => {
+  if (!cityName.value) {
+    return;
+  }
+  toggleModal("location");
+  navigateTo(`/city/${cityName.value}/car/${route.params.maker}`);
+  cityName.value = "";
+};
+</script>
+
 <template>
   <div>
     <!-- Car side bar -->
@@ -6,13 +31,27 @@
         class="p-5 flex justify-between relative cursor-pointer custom-border-bottom"
       >
         <h3>Location</h3>
-        <h3 class="capitalize text-blue-400 font-bold">Torronto</h3>
+        <h3
+          @click="toggleModal('location')"
+          class="capitalize text-blue-400 font-bold"
+        >
+          <!-- {{ cityName }} -->
+          kolkata
+        </h3>
 
         <!-- Modal Popup -->
-        <!-- <div class="left-56 p-5 absolute border shadow top-1 -m-1 bg-white">
-              <input class="border p-1 rounded" type="text"/>
-              <button class="bg-blue-400 mt-2 w-full rounded text-white p-1">Apply</button>
-            </div> -->
+        <div
+          v-if="modal.location"
+          class="left-56 p-5 absolute border shadow top-1 -m-1 bg-white"
+        >
+          <input class="border p-1 rounded" type="text" v-model="cityName" />
+          <button
+            @click="updateModal"
+            class="bg-blue-400 mt-2 w-full rounded text-white p-1"
+          >
+            Apply
+          </button>
+        </div>
       </div>
 
       <div
